a775184df30b4d396ed7f1221ed8ef2fcd530b9c
mibarg <michaelbargury@gmail.com>
2018-03-22 23:35:12 +0200

    or-michael dbg

--- detection/trajectory.py
+++ detection/trajectory.py
@@ -8,17 +8,17 @@ def initial_velocity(positions, dt):
 def trajectory_per_frame(positions, fps):
     epsilon = 0.0000001 * positions[0,1]
     dt = np.true_divide(1.0, fps)
-    next_position = np.copy(positions[-1])
     v = initial_velocity(positions, dt)[1]
-    trajectory = np.array([next_position])
+    next_position = np.copy(positions[-1, :])
+    trajectory = np.asarray([next_position, ])
     acceleration = np.array([0,-980,0])
     while next_position[1] > epsilon:
         next_position += v * dt
         v += acceleration * dt
-        trajectory = np.append(trajectory, next_position)
-    if trajectory[-1] < 0:
-        trajectory[-1] = 0
-    return np.reshape(trajectory, (int(np.floor(len(trajectory)/3)),3))
+        trajectory = np.r_[trajectory, next_position]
+    if trajectory[-1, 1] < 0:
+        trajectory[-1, 1] = 0
+    return trajectory
 
 
 def rotate_frame(positions, normal):
@@ -26,13 +26,10 @@ def rotate_frame(positions, normal):
     rotation_matrix = np.array([[np.cos(phi),0,-np.sin(phi)],
                                 [0,1,0],
                                 [np.sin(phi),0,np.cos(phi)]])
-    rotated_positions = np.asarray([])
-    print(rotation_matrix)
+    rotated_positions = np.asarray([[0, 0, 0], ])
     for row in positions:
-        print(np.transpose(row))
-        rotated_positions = np.append(rotated_positions, np.matmul(rotation_matrix, row))
-    return np.reshape(rotated_positions,
-     (int(np.floor(len(rotated_positions)/3)),3))
+        rotated_positions = np.r_[rotated_positions, np.matmul(rotation_matrix, row).reshape((1, 3))]
+    return rotated_positions[1:, :]
 
 
 def find_trajectory(positions, normal, fps):
