5beea7df7f032c00d06d445c319f1ab303d37fa1
dsh0 <33100676+dsh0@users.noreply.github.com>
2018-03-22 20:50:18 +0200

    locate_ball now fast & detects Big Balls

--- detection/locate_ball_2d.py
+++ detection/locate_ball_2d.py
@@ -3,25 +3,48 @@ from scipy import signal
 
 def find_xyr(im):
     # main function. gets an R*C or R*C*3 image and returns a triple (x,y,r) or strongest circle
-    ed=edge(im)
-    max_lim=min(im.shape[0],im.shape[1],20)
-    L=50
-    rads=np.linspace(5,max_lim,L)
+    ed1=maxpool(edge(im))
+    ed2=maxpool(ed1)
+    ed3=maxpool(ed2)
+    eds=[ed3,ed2]
+    imsiz=min(ed3.shape)
+    rads=np.linspace(imsiz/5,imsiz/2,10)
+    for i,ed in enumerate(eds):
+        #print('~~~phase~~~~')
+        xyr=scan_xyr(ed,rads)
+        r=rads[xyr[2]]*2
+        rads=np.linspace(r*.7, r*1.3, round(4/(1+i)))
+    return (2*xyr[0],2*xyr[1],2*r)
+        
+
+def scan_xyr(im,rlist):
+    L=len(rlist)
     xy=np.zeros((L,2))
     maxgr=np.zeros(L)
     for i in range(L):
-        filt=circle_filter(rads[i])
+        #print('make filter')
+        filt=circle_filter(rlist[i])
+        #print('convolute!',im.shape,rlist[i],filt.shape)
         grade=signal.convolve2d(im,filt)
         xy[i]=np.unravel_index(grade.argmax(), grade.shape)-(np.array(filt.shape)-3)/2
         maxgr[i]=np.max(grade)
     imax=maxgr.argmax()
-    return (*xy[imax],rads[imax]+.5)
+    return (*xy[imax],imax)
+    
+def maxpool(im):
+    if im.shape[0]%2:
+        im=im[:-1,:]
+    if im.shape[1]%2:
+        im=im[:,:-1]
+    im=np.maximum(im[::2,:],im[1::2,:])
+    return np.maximum(im[:,::2],im[:,1::2])
 
 def edge(im):
     # assumes im.shape==(height,length,1/3)
     # YUV/grayscale format
     if len(im.shape)==3:
         im=im[:,:,0]
+    im=signal.convolve2d(im,np.ones((3,3),np.float64))
     dx = np.abs(im[1:,:]-im[:-1,:])
     dy = np.abs(im[:,1:]-im[:,:-1])
     dx2=dx[1:,:]+dx[:-1,:]
